ed8b302d5b66288c72d7905103dd7f534a1f70fc
Andrew Marusin <hoomer@bk.ru>
2015-09-10 15:07:04 +0300

    D2 step on page 146 was been done!

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,10 @@ class StoreController < ApplicationController
   def index
   	@products = Product.order(:title)
   	@leftbartime = Time.now
+  	if session[:counter].nil?
+  		session[:counter] = 1
+  	else
+  		session[:counter] += 1
+  	end
   end
 end
