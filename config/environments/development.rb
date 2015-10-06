cbfb4197c8baeb753fcdd2fefbe0863ecd307e40
Andrew Hoomer <hoomer@bk.ru>
2015-10-06 17:00:15 +0400

    I was stopped on the p.203

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,9 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  #Depot::Application.configure do
+  #  config.action_mailer.delivery_method = :test
+  #end
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
