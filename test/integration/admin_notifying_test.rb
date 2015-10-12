b06d7f33d91509040420fbb7cdf234a0df5f8721
Andrew Hoomer <hoomer@bk.ru>
2015-10-12 16:54:26 +0400

    Autorization added, but complex test failed on 302 error page.

--- test/integration/admin_notifying_test.rb
+++ test/integration/admin_notifying_test.rb
@@ -1,14 +1,23 @@
 require 'test_helper'
 
 class AdminNotifyingTest < ActionDispatch::IntegrationTest
-	test "notifying to admin about unexisting cart access error" do
-		Cart.delete_all
+	fixtures :carts, :users
 
-  		get "/"
+	test "notifying to admin about unexisting cart access error" do
+		
+		get "/"
 		assert_response :success
 		assert_template "index"
 		
-		get "/carts/1/edit"
+		session[:user_id] = users(:one).id
+		byebug
+        get "/carts/#{carts(:one).id}/edit"
+		byebug
+		assert_response :success
+		
+		get "/carts/1/edit" 
+		
+
 		assert_response :redirect
 		assert_redirected_to store_path
 		
