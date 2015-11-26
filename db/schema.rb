099b3b315f54b862f160756bffecebfdce309a74
Andrew Marusin <hoomer@bk.ru>
2015-11-26 14:18:47 +0300

    Capistrano for rbenv test

--- db/schema.rb
+++ db/schema.rb
@@ -43,9 +43,9 @@ ActiveRecord::Schema.define(version: 20151025175519) do
     t.string   "name"
     t.text     "address"
     t.string   "email"
+    t.string   "pay_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "pay_type"
     t.datetime "ship_date"
   end
 
